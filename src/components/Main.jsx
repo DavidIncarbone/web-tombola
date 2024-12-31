3edfbb8bc95ac04b485bbb05dde49d45999f0070
DavidIncarbone <david.incarbone@gmail.com>
2024-12-31 17:26:01 +0100

    end graphic milestone

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,18 +5,20 @@ import numeri from "../data/numbers.js"
 function Main() {
 
     return (
-        <main className="d-flex">
-            <div className="container w-50 d-flex">
-                <div className="row d-flex g-2">
+        <main>
+            <div className="container w-75 d-flex justify-content-center">
+                <div className="row d-flex g-2 w-50">
                     {numeri.map((numero) => <Number key={crypto.randomUUID()} number={numero} />)}
 
                 </div>
-                <div>
-                    <h5>Ultimo numero estratto</h5>
-                    <div></div>
+                <div className="w-25">
+                    <h5 className="text-center">Ultimo numero estratto</h5>
+                    <div id="result"></div>
                     <hr />
-                    <button>Estrai</button>
-                    <button>Termina gioco</button>
+                    <div className="d-flex flex-column justify-content-center align-items-center gap-2">
+                        <button id="estrai" className="px-3">Estrai</button>
+                        <button id="termina-gioco" className="px-3">Termina gioco</button>
+                    </div>
                 </div>
 
             </div>
--- src/components/Number.jsx
+++ src/components/Number.jsx
@@ -1,10 +1,10 @@
-
+import NumberStyle from "../style/Numbers.module.css"
 
 function Number({ number, id }) {
 
     return (
-        <div key={id} id="number" className="m-2">
-            {number}
+        <div key={id} id="number" className="m-1">
+            <b>{number}</b>
         </div>
     )
 }

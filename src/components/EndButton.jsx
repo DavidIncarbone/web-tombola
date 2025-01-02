1cffb1e4817b63d68cd8d703d581c179d097a408
DavidIncarbone <david.incarbone@gmail.com>
2025-01-02 15:08:24 +0100

    add extraction system

---
+++ src/components/EndButton.jsx
@@ -0,0 +1,5 @@
+function EndButton({ reset }) {
+    return <button id="termina-gioco" className="px-3" onClick={reset}>Termina gioco</button>
+}
+
+export default EndButton
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,16 +2,28 @@ import { useState, useEffect } from "react";
 import Number from "./Number";
 import numeri from "../data/numbers.js";
 import ExtractButton from "./ExtractButton.jsx";
+import numbersStyle from "../style/Numbers.module.css";
+import EndButton from "./EndButton.jsx";
 
 
 function Main() {
+    const [numbersCopy, setNumbersCopy] = useState(numeri);
+    const [extracted, setExtracted] = useState(null);
+    function extractor() {
+        if (numbersCopy.length > 0) {
+            const number = Math.floor(Math.random() * numbersCopy.length);
+            const extractedNumber = numbersCopy[number];
+            setExtracted(extractedNumber)
+            setNumbersCopy(numbersCopy.filter((numero) => numero !== extractedNumber))
+        } else {
 
-    const [extractedNumber, setExtractedNumber] = useState();
+            alert("Tutti i numeri sono stati estratti!")
+        }
+    }
 
-
-
-    function extractor(max) {
-        setExtractedNumber(Math.floor(Math.random() * max + 1));
+    function endGame() {
+        setExtracted(null);
+        setNumbersCopy(numeri);
     }
 
     return (
@@ -24,12 +36,15 @@ function Main() {
                 <div className="w-25">
                     <h5 className="text-center">Ultimo numero estratto</h5>
                     <div id="result" className="d-flex justify-content-center align-items-center">
-                        <b>{extractedNumber}</b>
+                        <b>{extracted}</b>
                     </div>
                     <hr />
                     <div className="d-flex flex-column justify-content-center align-items-center gap-2">
-                        <ExtractButton extractor={() => extractor(90)} />
-                        <button id="termina-gioco" className="px-3">Termina gioco</button>
+                        <ExtractButton extractor={() => {
+                            extractor()
+                            setTimeout(console.log(numbersCopy), 1000)
+                        }} />
+                        <EndButton reset={() => endGame()} />
                     </div>
                 </div>
 
